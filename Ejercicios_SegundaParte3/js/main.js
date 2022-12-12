e469393e2502f6470a8bae7a6295ecfec9237c95
David <davidesv69@gmail.com>
2022-12-12 11:03:33 -0600

    ligero cambio en los comentarios

--- Ejercicios_SegundaParte3/js/main.js
+++ Ejercicios_SegundaParte3/js/main.js
@@ -25,9 +25,9 @@ function checkGuess() {
                  btnIntentos.disabled = true;
                 }else if (num < randomNumber) {
         
-                    return resultado.innerHTML = `El número es muy bajo! te quedan: ${intentos}`;
+                    return resultado.innerHTML = `El número es muy bajo! te quedan: ${intentos} intentos`;
                 }else if (num > randomNumber) {
-                    return resultado.innerHTML = `El número es muy alto! te quedan: ${intentos}`;
+                    return resultado.innerHTML = `El número es muy alto! te quedan: ${intentos} intentos`;
                 }
 
             } while (randomNumber != num && intentos > 0);
@@ -41,16 +41,3 @@ function checkGuess() {
 btnIntentos.addEventListener("click", checkGuess)
 
 
-
-/*
-if (num == randomNumber) {
-                return resultado.innerHTML = `Felicidades, lo has adivinado!`;
-
-            }else if (intentos == 0) {
-                return resultado.innerHTML = `Se te acabaron los intentos`;
-            }else if (num < randomNumber) {
-    
-                return resultado.innerHTML = `El número es muy bajo!`;
-            }else if (num > randomNumber) {
-                return resultado.innerHTML = `El número es muy alto!`;
-            } */
